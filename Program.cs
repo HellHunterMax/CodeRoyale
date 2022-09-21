9d41b05d59ebc11d4383e313dd6ab439bf5296fc
Max <hellhunter15@hotmail.com>
2022-09-21 22:42:13 +0200

    refactor: use gamestates

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ using System.Linq;
 //net5.0-windows-v1.0\CSharpSourcesToSingleFile.exe -d C:\Repo\CodinGame\CodeRoyale -o merge.cs
 //Console.Error.WriteLine($"");
 //Console.Error.WriteLine($"");
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+// Write an action using Console.WriteLine()
+// To debug: Console.Error.WriteLine("Debug messages...");
 
-            // First line: A valid queen action
-            // Second line: A set of training instructions
+// First line: A valid queen action
+// Second line: A set of 
